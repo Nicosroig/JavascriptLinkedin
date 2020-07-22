791748bd6ac69ae9f7c98e80eb159ab78106a112
Nicosroig <nicosroig@gmail.com>
2020-07-22 11:28:26 -0300

    Search commit

--- 11 - Trabajando con cadenas de texto/3 - Métodos de busqueda parte 1/js/app.js
+++ 11 - Trabajando con cadenas de texto/3 - Métodos de busqueda parte 1/js/app.js
@@ -9,18 +9,25 @@ var resultado;
 
 // indexOf
 // resultado = mensaje.indexOf("aprendiendo");
+//busca la posicion de la primer ocurrencia de lo que estamos buscando
 
 
 // lastIndexOf
 //resultado = mensaje.lastIndexOf("aprendiendo");
+//busca la posicion de la ultima ocurrencia de la palabra buscada
 
 
 // search:
 //resultado = mensaje.search("aprendiendo");
+//similar a indexOf, busca la primera ocurrencia
 
 
 // search | Expresión regular
 resultado = mensaje.search(/ja/i);
+//busca la expresion. la i es in case sensitive para mayusculas y minusculas
+
+
+//Cualquiera de estas expresiones si dan -1 es porque no encontraron nada
 
 
 console.log(resultado);
